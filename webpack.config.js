a25eeb9b7624a49da5213e803eaaa6344599eb39
Chandler Fang <cfang@jadeworld.com>
2018-01-30 11:10:19 +1100

    babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            }
+        ]
     }
  };
 
